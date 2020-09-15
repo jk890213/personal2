ea205a353abba10dafe32260cb1c9273b81639c1
jk890213 <jk890213@gmail.com>
2020-09-15 22:13:32 +0800

    修正

--- js/teach.js
+++ js/teach.js
@@ -1,4 +1,4 @@
-var container = document.getElementsByClassName("container");
+/* var container = document.getElementsByClassName("container");
 for(let i = 0; i < container.length; i++){
     container[i].addEventListener("click", function(){   
         this.style.height = "auto";        
@@ -12,4 +12,31 @@ for(let i = 0; i < container.length; i++){
         
     });
 };
+*/
+
+$(document).ready(function(){
+
+    
+    var control = document.getElementsByClassName("control");
+    for(let i = 0; i < control.length; i++){
+        $(control[i]).click(function(){
+            let txt = document.getElementsByClassName("txt");
+            $(txt[i]).slideToggle();
+            for(j = 0; j < txt.length; j++){
+                if(txt[j] != txt[i] && txt[j].classList.contains("-on")){
+                    $(txt[j]).slideUp();
+                };
+            };
+            txt[i].classList.add("-on");            
+        });
+    };
+
+    // hover浮動效果 mouseenter + leave + if(-on)
+
+    //側欄scrool文章
+
+    //文章展開跟隨標題
+
+
+})
 
