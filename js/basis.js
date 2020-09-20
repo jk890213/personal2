abb0f8f7e0ba4d23e96524218fecf18945a5caef
jk890213 <jk890213@gmail.com>
2020-09-20 11:04:08 +0800

    nav rwd

--- js/basis.js
+++ js/basis.js
@@ -1,11 +1,20 @@
-// jQuery 版本：DOM 載入完成之後執行
-$(function(){
-
-    // hamburger icon 的切換
-    $("button.hamburger").on("click", function(){
-        $(this).toggleClass("is-active");
-        let nav = document.getElementsByClassName("nav");
-        nav[0].style.transform = "translate 0";
-    });
+var hamburg = document.getElementsByClassName('hamburg')[0];
+hamburg.addEventListener('click', function() {
+	let span1 = document.getElementsByClassName('span1')[0];
+	let span2 = document.getElementsByClassName('span2')[0];
+	let span3 = document.getElementsByClassName('span3')[0];
+	span1.classList.toggle('move1');
+	span2.classList.toggle('move2');
+	span3.classList.toggle('move3');
+	let nav = document.getElementsByClassName("nav")[0];
+	nav.classList.add("-transition");
+	if(nav.classList.contains("-translate")){
+		nav.classList.remove("-translate");
+		setTimeout(function(){
+			nav.classList.remove("-transition");
+		}, 500);
+    }else{
+		nav.classList.add("-translate");
+	}
 
 });
