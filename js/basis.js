52fe4a72fa13c6af2345df48f4aae090ee0d1c53
jk890213 <jk890213@gmail.com>
2020-09-29 14:15:34 +0800

    顧問rwd

--- js/basis.js
+++ js/basis.js
@@ -1,30 +1,40 @@
-var hamburg = document.getElementsByClassName('hamburg')[0];
-hamburg.addEventListener('click', function() {
-	let span1 = document.getElementsByClassName('span1')[0];
-	let span2 = document.getElementsByClassName('span2')[0];
-	let span3 = document.getElementsByClassName('span3')[0];
-	span1.classList.toggle('move1');
-	span2.classList.toggle('move2');
-	span3.classList.toggle('move3');
-	let nav = document.getElementsByClassName("nav")[0];
-	nav.classList.add("-transition");
-	if(nav.classList.contains("-translate")){
-		nav.classList.remove("-translate");
-		setTimeout(function(){
-			nav.classList.remove("-transition");
-		}, 500);
-    }else{
-		nav.classList.add("-translate");
-	}
+$(document).ready(function(){
+
+	var hamburg = document.getElementsByClassName('hamburg')[0];
+	hamburg.addEventListener('click', function() {
+		let span1 = document.getElementsByClassName('span1')[0];
+		let span2 = document.getElementsByClassName('span2')[0];
+		let span3 = document.getElementsByClassName('span3')[0];
+		span1.classList.toggle('move1');
+		span2.classList.toggle('move2');
+		span3.classList.toggle('move3');
+		let nav = document.getElementsByClassName("nav")[0];
+		nav.classList.add("-transition");
+		if(nav.classList.contains("-translate")){
+			nav.classList.remove("-translate");
+			setTimeout(function(){
+				nav.classList.remove("-transition");
+			}, 500);
+		}else{
+			nav.classList.add("-translate");
+		}
+	});
+
+	//為何不能停止預設行為?
+	var noa = document.getElementsByClassName("-on")[0];
+	// noa.addEventListener("click", function(){
+	// 	alert("hi");
+	// });
+
+	noa.addEventListener("click", function(e){
+		e.preventDeault();
+	});
+
+
+
+
 });
 
-//為何不能停止預設行為?
-var noa = document.getElementsByClassName("-on")[0];
-// noa.addEventListener("click", function(){
-// 	alert("hi");
-// });
-noa.addEventListener("click", function(e){
-	if(noa.classList.contains("-on")){
-		e.preventDeault();
-	}
-});
+
+
+
--- js/consultant.js
+++ js/consultant.js
@@ -1,39 +1,6 @@
 var controller = new ScrollMagic.Controller();
+
 var an_talk = new TimelineMax();
-
-// an_talk.to('.no1', 1, {
-//     // scale: 1
-//     y: -200
-
-// }).to('.background', 1, {
-//     scale: 2  
-
-// }).to('.no2', 1, {
-//     // scale: 1
-//     y: -300
-
-// }).to('.background', 1, {
-//     scale: 1.2   
-
-// }).to('.no3', 1, {
-//     // scale: 1
-//     y: -400
-
-// }).to('.background', 1, {
-//     scale: 1.3   
-
-// }).to('.no4', 1, {
-//     // scale: 1
-//     y: -500
-
-// }).to('.background', 1, {
-//     scale: 1.4    
-
-// }).to('.no5', 1, {
-//     // scale: 1
-//     y: -600
-
-// });
 
 
 an_talk.to('.no1', 1, {
@@ -81,16 +48,61 @@ an_talk.to('.no1', 1, {
 });
 
 
+// an_talk.to('.no1', 1, {
+//     opacity: 1
+
+// }).to('.background', 4, {
+//     scale: 1.2
+
+// }).to('.no1', 1, {
+//     opacity: 0
+
+// }).to('.no2', 1, {
+//     opacity: 1
+
+// }).to('.background', 5, {
+//     scale: 1.35 
+
+// }).to('.no2', 1, {
+//     opacity: 0
+
+// }).to('.no3', 1, {
+//     opacity: 1
+
+// }).to('.background', 5, {
+//     scale: 1.5
+
+// }).to('.no3', 1, {
+//     opacity: 0
+
+// }).to('.no4', 1, {
+//     opacity: 1
+
+// }).to('.background', 5, {
+//     scale: 1.6 
+
+// }).to('.no4', 1, {
+//     opacity: 0
+
+// }).to('.no5', 1, {
+//     opacity: 1
+
+// }).to('.button', 1, {
+//     opacity: 1
+
+// });
+
+
 // var an_background = TweenMax.to('.background', 1, {   
 //     scale: 2
 // });
-//沒有給觸發場景 動畫會直接觸發
+// 沒有給觸發場景 動畫會直接觸發
 
 var scence1 = new ScrollMagic.Scene({
     triggleElement: '.wrap',
     triggleHook: 0,
     duration: '1000%'
-}).setPin('.wrap').setTween(an_talk).addIndicators().addTo(controller);
+}).setPin('.wrap').setTween(an_talk).addTo(controller);
 
 
 // var scence1 = new ScrollMagic.Scene({
@@ -101,24 +113,9 @@ var scence1 = new ScrollMagic.Scene({
 
 
 
-
-
 // var scence1 = new ScrollMagic.Scene({
 //     triggleElement: '.wrap',
 //     triggleHook: 0,
 //     duration: '500%'
 // }).setPin('.wrap').setTween(an_background).setTween(an_talk).addIndicators().addTo(controller);
 //setTween 只能有一個
-
-
-// var an_background = TweenMax.fromTo('.background', 1, {
-//     scale: 1
-// }, {
-//     scale: 2
-// });
-
-
-
-
-
-
